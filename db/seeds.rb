9eba4cf91be58c662565c3526dcccd819d92847f
Mike Madsen <mikewmadsen@gmail.com>
2022-11-04 14:04:43 -0700

    remove transactions from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,26 +7,23 @@
 #   Character.create(name: "Luke", movie: movies.first)
 require "open-uri"
 
-ApplicationRecord.transaction do 
-  puts "Destroying tables..."
-  # Unnecessary if using `rails db:seed:replant`
-  User.destroy_all
+puts "Destroying tables..."
+# Unnecessary if using `rails db:seed:replant`
+User.destroy_all
 
-  puts "Resetting primary keys..."
-  # For easy testing, so that after seeding, the first `User` has `id` of 1
-  ApplicationRecord.connection.reset_pk_sequence!('users')
+puts "Resetting primary keys..."
+# For easy testing, so that after seeding, the first `User` has `id` of 1
 
-  puts "Creating users..."
-  # Create one user with an easy to remember username, email, and password:
-  user = User.create!(
-    username: 'Demo-lish', 
-    email: 'demo@user.io', 
-    password: 'password'
-  )
+puts "Creating users..."
+# Create one user with an easy to remember username, email, and password:
+user = User.create!(
+  username: 'Demo-lish', 
+  email: 'demo@user.io', 
+  password: 'password'
+)
 
-  file = URI.open("https://datcord-seeds.s3.us-west-1.amazonaws.com/datcorddefault.jpeg")
-  user.photo.attach(io: file, filename: "default")
+file = URI.open("https://datcord-seeds.s3.us-west-1.amazonaws.com/datcorddefault.jpeg")
+user.photo.attach(io: file, filename: "demo")
 
 
-  puts "Done!"
-end
+puts "Done!"
